a7b5790a040fa23847ef8a8496a62016b9d6e529
renedis <renedis@hotmail.com>
2025-05-16 20:45:46 +0200

    Update sensor-bars-card.js

--- dist/sensor-bars-card.js
+++ dist/sensor-bars-card.js
@@ -107,52 +107,32 @@ class SensorBarsCard extends HTMLElement {
       const labelPosition = bar.label_position || "above";
       const valuePosition = bar.value_position || "right";
 
-      // Fix layout for (label:left + value:above) - use vertical stack with label left above bar
+      if (labelPosition === "none" && valuePosition === "right") {
+        return `
+          <div class="bar-row left" style="justify-content: flex-start;">
+            <div class="bar-container" style="background-color: ${bg}; height: ${height}px; width: calc(100% - 60px);">
+              <div class="bar-fill" style="width: ${percent}%; background-color: ${color};"></div>
+            </div>
+            ${showValue ? `<div class="value" style="width: 60px; text-align: right; align-self: center;">${value}${unit}</div>` : ''}
+          </div>
+        `;
+      }
+
       if (labelPosition === "left" && valuePosition === "above") {
         return `
           <div class="bar-row vertical-stack">
-            <div class="label left">
-              ${bar.icon ? `<ha-icon class="icon" icon="${bar.icon}"></ha-icon>` : ''}
-              <span>${bar.name}</span>
+            <div class="label-value-above" style="justify-content: space-between; width: 100%; margin-bottom: 4px;">
+              <div class="label left">
+                ${bar.icon ? `<ha-icon class="icon" icon="${bar.icon}"></ha-icon>` : ''}
+                <span>${bar.name}</span>
+              </div>
+              ${showValue ? `<div class="value" style="text-align: right;">${value}${unit}</div>` : ''}
             </div>
-            ${showValue ? `<div class="value" style="text-align: right; margin-bottom: 4px;">${value}${unit}</div>` : ''}
             <div class="bar-container" style="background-color: ${bg}; height: ${height}px;">
               <div class="bar-fill" style="width: ${percent}%; background-color: ${color};"></div>
             </div>
           </div>
         `;
-      }
-
-      // Fix layout for label:none with value:right or value:above
-      if (labelPosition === "none") {
-        if (valuePosition === "right") {
-          return `
-            <div class="bar-row" style="justify-content: flex-start;">
-              <div class="bar-container" style="background-color: ${bg}; height: ${height}px; width: calc(100% - 60px);">
-                <div class="bar-fill" style="width: ${percent}%; background-color: ${color};"></div>
-              </div>
-              ${showValue ? `<div class="value" style="width: 60px; text-align: right;">${value}${unit}</div>` : ''}
-            </div>
-          `;
-        } else if (valuePosition === "above") {
-          return `
-            <div class="bar-row vertical-stack">
-              ${showValue ? `<div class="value" style="text-align: right; margin-bottom: 4px;">${value}${unit}</div>` : ''}
-              <div class="bar-container" style="background-color: ${bg}; height: ${height}px;">
-                <div class="bar-fill" style="width: ${percent}%; background-color: ${color};"></div>
-              </div>
-            </div>
-          `;
-        } else {
-          // fallback: just bar full width
-          return `
-            <div class="bar-row">
-              <div class="bar-container" style="background-color: ${bg}; height: ${height}px;">
-                <div class="bar-fill" style="width: ${percent}%; background-color: ${color};"></div>
-              </div>
-            </div>
-          `;
-        }
       }
 
       // Compose label and value HTML for above together
