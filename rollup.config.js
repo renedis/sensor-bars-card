d0d5347bea9cd0a765bb3c9fa6803f26bbfe5839
renedis <renedis@hotmail.com>
2025-05-16 19:30:29 +0200

    Add files via upload

---
+++ rollup.config.js
@@ -0,0 +1,14 @@
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+
+export default {
+  input: 'src/sensor-bars-card.ts',
+  output: {
+    file: 'dist/sensor-bars-card.js',
+    format: 'es'
+  },
+  plugins: [
+    resolve(),
+    typescript()
+  ]
+};
